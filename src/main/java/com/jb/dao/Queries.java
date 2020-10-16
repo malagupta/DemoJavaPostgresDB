9232a6d332f0bb1e13946f5348dd55974835adcf
Mala Gupta <mala.gupta@gmail.com>
2020-10-16 16:16:09 +0530

    Queries as simple String values

---
+++ src/main/java/com/jb/dao/Queries.java
@@ -0,0 +1,44 @@
+package com.jb.dao;
+
+import com.jb.vo.Actor;
+
+public class Queries {
+    String updateActorUsingParameter(Actor actor) {
+        String query = String.format("""
+                                     UPDATE actor \
+                                     SET first_name = '%s' \
+                                     AND last_name = '%s' \
+                                     WHERE actor_id = %d \
+                                     """, actor.getFirstName(),
+                                          actor.getLastName(),
+                                          actor.getId());
+        return query;
+    }
+
+    String updateActorNoParameters() {
+        String query = """
+                       UPDATE actor 
+                       SET first_name = 'Joe', last_name = 'Annam'
+                       WHERE actor_id = 145
+                       """;
+        return query;
+    }
+
+    String simpleSelect() {
+        String query = """
+                       SELECT first_name, 
+                              last_name
+                       FROM actor
+                       WHERE first_name LIKE 'A%'
+                       """;
+        return query;
+    }
+
+    String simpleSelectQuery() {
+        String query = "SELECT first_name, " +
+                       "last_name " +
+                       "FROM actor " +
+                       "WHERE first_name LIKE '%A'";
+        return query;
+    }
+}
